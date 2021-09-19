c6862051112b4155fa6446f79fc37f325d548718
GuilhermeJWT <guiromanno@gmail.com>
2021-09-19 12:10:12 -0300

    Setup Inicial

---
+++ src/main/java/br/com/systemsgs/SqsSpringbootAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemsgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsSpringbootAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsSpringbootAwsApplication.class, args);
+	}
+
+}
