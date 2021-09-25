97c4cb32c19177abba969accc47cdf68d65190dd
GuilhermeJWT <guiromanno@gmail.com>
2021-09-25 12:51:34 -0300

    Adicionando o Logger e obsevando os Logs da fila

--- src/main/java/br/com/systemsgs/controller/SQSController.java
+++ src/main/java/br/com/systemsgs/controller/SQSController.java
@@ -1,8 +1,11 @@
 package br.com.systemsgs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/sqs")
 public class SQSController {
 
+    Logger logger = LoggerFactory.getLogger(SQSController.class);
+
     @Autowired
     private QueueMessagingTemplate queueMessagingTemplate;
 
@@ -22,6 +27,11 @@ public class SQSController {
     @GetMapping(value = "/{message}")
     public void sendMessageToQueue(@PathVariable String message){
         queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
+    }
+
+    @SqsListener("guilherme-sqs") /*nome da fila lá no SQS da AWS*/
+    public void loadMessageFromSQS(String message){
+        logger.info("message from SQS Queue {}", message);
     }
 
 }
